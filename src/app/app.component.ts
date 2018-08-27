66e0ba8b250950005d513b4066957483b1cc9141
ThomasHWeb <thomasholbing@gmail.com>
2018-08-27 23:52:36 +0200

    poste-project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'post-project';
+  title = 'Posts';
+
+  posts = [
+    {
+      title: 'Mon premier post',
+      content: 'Illius mare montis abstractas quas quas quas pedes Gnaeus Orontes Gnaeus pedes funditur Pompeius quas provincias pedes Orontes imosque mare celsi imosque ambiens regnis pedes praetermeans Cassii Parthenium funditur Romanae.',
+      loveIts: 4,
+      created_at: new Date()
+    },
+    {
+      title: 'Mon deuxième post',
+      content: 'Illius mare montis abstractas quas quas quas pedes Gnaeus Orontes Gnaeus pedes funditur Pompeius quas provincias pedes Orontes imosque mare celsi imosque ambiens regnis pedes praetermeans Cassii Parthenium funditur Romanae.',
+      loveIts: -2,
+      created_at: new Date()
+    },
+    {
+      title: 'Encore un post',
+      content: 'Illius mare montis abstractas quas quas quas pedes Gnaeus Orontes Gnaeus pedes funditur Pompeius quas provincias pedes Orontes imosque mare celsi imosque ambiens regnis pedes praetermeans Cassii Parthenium funditur Romanae.',
+      loveIts: 0,
+      created_at: new Date()
+    }
+  ];
 }
---
+++ src/app/post-list-item/post-list-item.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-list-item',
+  templateUrl: './post-list-item.component.html',
+  styleUrls: ['./post-list-item.component.scss']
+})
+export class PostListItemComponent implements OnInit {
+  @Input() post;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  addLove() {
+    this.post.loveIts++;
+  }
+
+  reduceLove() {
+    this.post.loveIts--;
+  }
+}
